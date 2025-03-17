534e896524a1448bc5884ec5bc331d6dbe9b8015
Styvalrev <syar@live.fr>
2025-03-17 20:57:03 +0100

    Update

---
+++ my_first_streamlit.py
@@ -0,0 +1,64 @@
+### Création du fichier Python pour Streamlit
+
+# question (h)
+import streamlit as st
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Embedding, Dense, GlobalAveragePooling1D
+
+# question (i)
+
+st.title("Modèle Word2Vec")
+
+# question (j)
+
+embedding_dim = 300
+model = Sequential()
+model.add(Embedding(vocab_size, embedding_dim))
+model.add(GlobalAveragePooling1D())
+model.add(Dense(vocab_size, activation='softmax'))
+
+model.load_weights("word2vec.h5")
+
+# question (k)
+
+vectors = model.layers[0].trainable_weights[0].numpy()
+import numpy as np
+from sklearn.preprocessing import Normalizer
+
+def dot_product(vec1, vec2):
+    return np.sum((vec1*vec2))
+
+def cosine_similarity(vec1, vec2):
+    return dot_product(vec1, vec2)/np.sqrt(dot_product(vec1, vec1)*dot_product(vec2, vec2))
+
+def find_closest(word_index, vectors, number_closest):
+    list1=[]
+    query_vector = vectors[word_index]
+    for index, vector in enumerate(vectors):
+        if not np.array_equal(vector, query_vector):
+            dist = cosine_similarity(vector, query_vector)
+            list1.append([dist,index])
+    return np.asarray(sorted(list1,reverse=True)[:number_closest])
+
+def compare(index_word1, index_word2, index_word3, vectors, number_closest):
+    list1=[]
+    query_vector = vectors[index_word1] - vectors[index_word2] + vectors[index_word3]
+    normalizer = Normalizer()
+    query_vector =  normalizer.fit_transform([query_vector], 'l2')
+    query_vector= query_vector[0]
+    for index, vector in enumerate(vectors):
+        if not np.array_equal(vector, query_vector):
+            dist = cosine_similarity(vector, query_vector)
+            list1.append([dist,index])
+    return np.asarray(sorted(list1,reverse=True)[:number_closest])
+
+def print_closest(word, number=10):
+    index_closest_words = find_closest(word2idx[word], vectors, number)
+    for index_word in index_closest_words:
+        print(idx2word[index_word[1]]," -- ",index_word[0])
+
+
+# question (l)
+
+#Exemple d'utilisation de la fonction print_closest
+print_closest('zombie')
