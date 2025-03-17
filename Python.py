c4156e16712538ecc5dd89e34f96185e0d05c3fa
Styvalrev <syar@live.fr>
2025-03-17 11:14:02 +0100

    Update Python.py

--- Python.py
+++ Python.py
@@ -7,3 +7,12 @@ a = generate_random_value(10)
 b = generate_random_value(20) 
 st.write(a) 
 st.write(b)
+
+joblib.load("model")
+loaded_model = pickle.load(open("model", 'rb'))
+
+import joblib
+joblib.dump(clf, "model")
+
+import pickle
+pickle.dump(clf, open("model", 'wb'))
